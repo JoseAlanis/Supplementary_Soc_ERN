e80c343cd3a18350f404c4c8945e778bdd1f3f17
José C. G. Alanis <joialanisson@gmail.com>
2018-05-15 16:09:39 +0200

    source data summary

--- R_Analysis_Scripts/01_analysis_of_errors.R
+++ R_Analysis_Scripts/01_analysis_of_errors.R
@@ -6,6 +6,7 @@
 source('~/Documents/GitHub/Supplementary_Soc_ERN/R_Functions/getPacks.R')
 source('~/Documents/GitHub/Supplementary_Soc_ERN/R_Functions/stdResid.R')
 source('~/Documents/GitHub/Supplementary_Soc_ERN/R_Functions/overDisp.R')
+source('~/Documents/GitHub/Supplementary_Soc_ERN/R_Functions/data_summary.R')
 
 # Install and load multiple R packages necessary for analysis.
 pkgs <- c('dplyr', 
