2cc5cafb88a658e2c6f4938caa61fdbfcf516eeb
José C. G. Alanis <joialanisson@gmail.com>
2018-09-03 19:25:20 +0200

    scale residuals (dont use)
    add argument for scaling pearson residuals (experimetal, dont use for now)

--- R_Functions/stdResid.R
+++ R_Functions/stdResid.R
@@ -1,13 +1,22 @@
-stdResid <- function(data, model, return.data = T, bound = 2.5, plot = F, show.bound = F, show.loess = F, ... ) {
+stdResid <- function(data, model, return.data = T, bound = 2.5, scale = F, plot = F, show.bound = F, show.loess = F, ... ) {
   
   # Save model name for later
   main <- deparse(substitute(model))
   
-  data <- data %>% 
-    # Compute standaridised residuals
-    mutate(st_R = as.numeric(scale(residuals( model, type = 'pearson' ))),
-           # Code cases as outliers if residual > bound
-           Outlier = ifelse(abs(st_R) > bound, 1, 0))
+  if (scale == T) {
+    data <- data %>% 
+      # Compute standaridised residuals
+      mutate(st_R = as.numeric(scale(residuals( model, type = 'pearson' ))),
+             # Code cases as outliers if residual > bound
+             Outlier = ifelse(abs(st_R) > bound, 1, 0))
+  } else {
+    data <- data %>% 
+      # Compute standaridised residuals
+      mutate(st_R = as.numeric(residuals( model, type = 'pearson' )),
+             # Code cases as outliers if residual > bound
+             Outlier = ifelse(abs(st_R) > bound, 1, 0))
+  }
+
   
   # summary of outlying cases
   print(paste(round((sum(data$Outlier == 1) / sum(data$Outlier == 0))*100, digits = 2), 
